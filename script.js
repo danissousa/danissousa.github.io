4a73a03acd6170de5d5cb527b03a84a272413bf2
danissousa <166165329+danissousa@users.noreply.github.com>
2025-05-09 18:29:01 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let contar = 0;
+function contarBeijos(){
+let resultado = document.getElementById("resultado")
+contar++
+resultado.innerHTML="O numero de beijos que estou te devendo " + contar
+
+}
